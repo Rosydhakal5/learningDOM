0f9d86844eb58091dcade2f5e14e0bc6e7d231c4
Rosy <rosydhakal333@gmail.com>
2023-12-07 18:35:55 +1100

    learning DOM

---
+++ file.js
@@ -0,0 +1,24 @@
+console.log("hello world!")
+ 
+//using getElementByTagName ----------->
+const heading = document.getElementsByTagName("h1");
+
+console.log(heading);
+
+
+
+
+
+const items = document.getElementsByClassName("item");
+console.log(items);
+
+const comment = document.getElementById("comments-section");
+console.log(comment);
+
+
+const paragraph = document.querySelector("p");
+console.log(paragraph);
+
+
+const para = document.querySelector(".para");
+console.log(para);
